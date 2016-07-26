a13c28fc1e8bda7b9a1bba20da07e7e73938be7e
Aakash <akash.bhatia1184@gmail.com>
2016-07-27 06:20:52 +1000

    Tracability interim

---
+++ wp-content/themes/maishawp-child/tracability.php
@@ -0,0 +1,23 @@
+<?php
+/*
+	Template Name: Tracability
+*/
+?>
+
+<?php get_header(); ?>
+	<main class="main" id="tracability" role="main">
+    	<a class="cotton-plant" href="#">Cotton Plant</a>
+        <img src="http://localhost/woodwose/wp-content/themes/maishawp-child/images/tracability/arrow.png" title="Arrow" alt="Arrow">
+        
+    	<a class="cloth" href="#">Cloth Roll</a>
+        <img src="http://localhost/woodwose/wp-content/themes/maishawp-child/images/tracability/arrow.png" title="Arrow" alt="Arrow">
+
+    	<a class="dye" href="#">Dying</a>
+        <img src="http://localhost/woodwose/wp-content/themes/maishawp-child/images/tracability/arrow.png" title="Arrow" alt="Arrow">
+
+    	<a class="stitch" href="#">Stitching</a>
+        <img class="divider" src="http://localhost/woodwose/wp-content/themes/maishawp-child/images/tracability/divider.png" title="Divider" alt="Divider">
+
+    	<a class="final-product" href="#">Final Product</a>
+    </main>
+<?php get_footer(); ?>
