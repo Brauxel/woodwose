b177b3f78b76fd5be08f023639cb37e786ff65ee
Aakash <akash.bhatia1184@gmail.com>
2016-07-27 23:37:49 +1000

    Tracability update

--- wp-content/themes/maishawp-child/tracability.php
+++ wp-content/themes/maishawp-child/tracability.php
@@ -5,6 +5,9 @@
 ?>
 
 <?php get_header(); ?>
+	<div class="heading" style="background-image:url(http://localhost/woodwose/wp-content/uploads/2015/04/maisha-about-top-image.jpg); margin-top:30px; padding-top:30px; padding-bottom:30px; font-size:6rem; color:#fff; text-align:center;">
+		<h1>Our Process</h1>
+    </div>
 	<main class="main" id="tracability" role="main">
     	<a class="cotton-plant" href="#">Cotton Plant</a>
         <img src="http://localhost/woodwose/wp-content/themes/maishawp-child/images/tracability/arrow.png" title="Arrow" alt="Arrow">
@@ -16,6 +19,7 @@
         <img src="http://localhost/woodwose/wp-content/themes/maishawp-child/images/tracability/arrow.png" title="Arrow" alt="Arrow">
 
     	<a class="stitch" href="#">Stitching</a>
+        <div class="clear"></div>
         <img class="divider" src="http://localhost/woodwose/wp-content/themes/maishawp-child/images/tracability/divider.png" title="Divider" alt="Divider">
 
     	<a class="final-product" href="#">Final Product</a>
